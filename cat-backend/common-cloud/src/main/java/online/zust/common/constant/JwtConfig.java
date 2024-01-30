f0bcce775e0a4383631c82260707b7eef5aed230
pqcqaq <905739777@qq.com>
2024-01-30 17:32:56 +0800

    修改配置，正常打包

--- cat-backend/common-cloud/src/main/java/online/zust/common/constant/JwtConfig.java
+++ cat-backend/common-cloud/src/main/java/online/zust/common/constant/JwtConfig.java
@@ -1,17 +1,14 @@
 package online.zust.common.constant;
 
 import lombok.Data;
-import online.zust.common.utils.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author qcqcqc
  */
 @Configuration
 @Data
-@PropertySource(value = "classpath:jwt-config.yml", encoding = "utf-8", factory = YamlPropertySourceFactory.class)
 public class JwtConfig {
     @Value("${jwt.secret}")
     private String secret;
